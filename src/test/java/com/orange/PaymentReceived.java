a8c95fd34ca060e73c5dbc541167e6ba11e1a444
Thyleshnandhu <thyleshnandhu98@gmail.com>
2023-09-30 16:11:43 +0530

    payment received commit

---
+++ src/test/java/com/orange/PaymentReceived.java
@@ -0,0 +1,11 @@
+package com.orange;
+
+import org.testng.annotations.Test;
+
+public class PaymentReceived {
+	@Test
+	public void paymentreceived() {
+		System.out.println("payment received");
+	}
+
+}
